83f7c77f502dee1c1b03bdf92b4c9026f51368fe
Truong Ngoc Chau <114427250+billtruong003@users.noreply.github.com>
2023-02-28 13:04:10 +0700

    Add files via upload
    This is to generate student feedback for midterm exam

---
+++ Midterm/Data.py
@@ -0,0 +1,98 @@
+feedback_test = {
+    "Good": [
+        "Học sinh làm bài tốt và tập trung. Có hiểu bài và nhớ bài tốt, tiếp tục phát huy con nhé",
+        "Học sinh có quá trình làm bài tốt, bên cạnh đó thể hiện được năng lực và khả năng hiểu bài.",
+        "Học sinh làm bài tốt và đã áp dụng được kiến thức vào bài làm.",
+        "Học sinh thể hiện sự chăm chỉ và nỗ lực trong quá trình học tập.",     
+        "Học sinh có sự tiến bộ rõ rệt trong việc hiểu và giải quyết các vấn đề bài thi đặt ra.",
+        "Học sinh đã hoàn thành bài kiểm tra một cách chính xác và đầy đủ.",
+        "Học sinh đã thực hiện bài kiểm tra nhanh chóng và hiệu quả.",
+        "Học sinh có sự khéo léo và sáng tạo trong cách giải quyết các vấn đề được đưa ra.",
+        "Học sinh đã thể hiện được sự tự tin và linh hoạt trong việc giải quyết các vấn đề khó khăn.",
+        "Học sinh có tinh thần cầu tiến cao, luôn muốn học hỏi thêm kiến thức mới."
+             ],
+    "Average": [
+        "Học sinh cần cải thiện kỹ năng làm bài để đạt điểm tốt hơn.",
+        "Học sinh cần chú ý đến từng chi tiết trong bài làm để không mắc phải lỗi sai nhỏ.",
+        "Học sinh cần cải thiện kỹ năng làm bài để đạt điểm trung bình.",
+        "Học sinh cần tham khảo thêm tài liệu để hiểu rõ hơn về bài học." 
+        "Học sinh cần thực hành nhiều hơn để cải thiện kỹ năng làm bài.",
+        "Học sinh có thể tham khảo thêm các tài liệu liên quan để nắm vững kiến thức.",
+        "Học sinh cần chú ý đến chi tiết để tránh mắc những lỗi thường gặp.",
+        "Học sinh cần tập trung hơn vào các vấn đề bài thi đặt ra để giải quyết các vấn đề khó khăn hiệu quả hơn.",
+        "Học sinh nên đọc kỹ yêu cầu của đề bài để tránh làm sai đáp án.",
+        "Học sinh cần chú ý đến cách trình bày bài làm để đạt điểm cao hơn.",
+        "Học sinh cần tìm hiểu kỹ hơn về những thuật ngữ mới trong bài học."
+                ],
+     "Need Improvement": [
+        "Học sinh cần thay đổi phương pháp học tập để có thể đạt được kết quả tốt hơn.",
+        "Học sinh cần đưa ra kế hoạch học tập cụ thể và thực hiện đúng để cải thiện kết quả.",
+        "Học sinh cần chú ý đến việc giải quyết các vấn đề cơ bản trong bài thi cũng như bài học để có thể cải thiện kết quả.",
+        "Học sinh cần lưu ý đến việc đọc và hiểu đề bài trước khi làm bài thi.",
+        "Học sinh cần tập trung hơn vào việc học và giải quyết các vấn đề trong bài học giáo viên giao.",
+        "Học sinh cần tìm hiểu kỹ hơn các kiến thức cơ bản để có thể đáp ứng được yêu cầu của bài làm.",
+        "Học sinh cần chú ý đến việc kiểm tra đầy đủ và chính xác bài làm để tránh sai sót.",
+        "Học sinh cần tham gia tích cực vào các hoạt động học tập để cải thiện kết quả."
+                        ],
+    "Poor": [
+        "Học sinh cần cải thiện kỹ năng học tập để đạt được kết quả tốt hơn.",
+        "Học sinh cần tập trung hơn và đưa ra kế hoạch học tập để đạt được kết quả tốt hơn.",
+        "Học sinh cần tham gia tích cực vào các hoạt động học tập để cải thiện kết quả.",
+        "Học sinh cần tìm hiểu lại các kiến thức cơ bản để có thể đáp ứng được yêu cầu của bài kiểm tra.",
+        "Học sinh cần cố gắng hơn để có thể giải quyết được các vấn đề trong làm.",
+        "Học sinh cần tìm hiểu kỹ hơn và làm bài cẩn thận để tránh sai sót.",
+        "Học sinh cần chú ý đến việc tổ chức thời gian làm để có thể hoàn thành bài kiểm tra và nộp bài đúng giờ.",
+        "Học sinh cần đọc và hiểu đề bài kỹ hơn để có thể thực hiện bài làm đúng yêu cầu."
+            ]
+   
+}
+
+feedback_practical = {
+    "Good": [
+        "Học sinh đã thực hiện bài tập thực hành tốt và hoàn thành đầy đủ yêu cầu.",
+        "Học sinh đã thể hiện được kỹ năng và sự nỗ lực trong việc thực hiện bài tập.",
+        "Học sinh đã làm tốt bài tập thực hành và chú ý đến từng chi tiết để đạt được kết quả tốt."
+    ],
+    "Average": [
+        "Học sinh đã hoàn thành bài tập thực hành, tuy nhiên cần cải thiện kỹ năng để đạt kết quả tốt hơn.",
+        "Học sinh cần tập trung hơn và chú ý đến việc thực hiện bài tập thực hành để cải thiện kết quả.",
+        "Học sinh cần tìm hiểu kỹ hơn về cách thực hiện bài tập thực hành để có thể làm tốt hơn."
+    ],
+    "Need improvement": [
+        "Học sinh cần đọc và hiểu rõ yêu cầu của bài tập thực hành trước khi bắt đầu làm.",
+        "Học sinh cần tìm hiểu kỹ hơn về các kiến thức cơ bản để có thể thực hiện bài tập thực hành tốt hơn.",
+        "Học sinh cần thực hành nhiều hơn để có thể cải thiện kỹ năng và đạt được kết quả tốt hơn."
+    ],
+    "Poor": [
+        "Học sinh cần cải thiện kỹ năng và chú ý đến việc thực hiện bài tập thực hành để đạt kết quả tốt hơn.",
+        "Học sinh cần tập trung hơn vào việc thực hiện bài tập thực hành và làm đúng yêu cầu của đề bài.",
+        "Học sinh cần tìm hiểu kỹ hơn về cách thực hiện bài tập thực hành để có thể đạt kết quả tốt hơn."
+    ]
+}
+
+general_feedback = {
+    "Good": [
+        "Học sinh đã thể hiện được sự tiến bộ trong học tập.",
+        "Học sinh đã hoàn thành bài tập đúng thời hạn.",
+        "Học sinh đã tập trung trong giờ học và đạt kết quả tốt.",
+        "Học sinh có thái độ tích cực và cố gắng hết sức trong học tập."
+    ],
+    "Average": [
+        "Học sinh cần phải cải thiện kỹ năng đọc hiểu để hiểu bài tốt hơn.",
+        "Học sinh cần phải tập trung hơn để không mắc phải những lỗi cơ bản.",
+        "Học sinh cần phải luyện tập thêm để nắm vững kiến thức.",
+        "Học sinh cần phải đặt mục tiêu học tập rõ ràng hơn."
+    ],
+    "Need improvement": [
+        "Học sinh cần phải chuẩn bị tốt hơn trước giờ học để có thể tham gia tích cực vào lớp.",
+        "Học sinh cần phải cải thiện kỹ năng viết để trình bày ý tưởng rõ ràng hơn.",
+        "Học sinh cần phải đọc kỹ đề bài trước khi làm bài tập.",
+        "Học sinh cần phải tham gia tích cực vào các hoạt động học tập để có thể rèn luyện kỹ năng."
+    ],
+    "Poor": [
+        "Học sinh cần phải cải thiện thái độ học tập để có thể đạt kết quả tốt hơn.",
+        "Học sinh cần phải tập trung hơn trong giờ học để không mắc phải những sai lầm cơ bản.",
+        "Học sinh cần phải nắm vững kiến thức cơ bản để có thể phát triển tiếp theo.",
+        "Học sinh cần phải thực hành nhiều hơn để rèn luyện kỹ năng."
+    ]
+}
